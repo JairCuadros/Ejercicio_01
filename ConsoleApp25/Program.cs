1e80ba45b6505d8bf34cfa5f217ad29263f1dec1
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-10-03 19:56:12 -0500

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese el valor de n: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("==================================");
+double resultadoF = 0;
+for (int i = 1; i <= n; i++)
+{
+    double suma = (16 - i) / (3.0 * i);
+    resultadoF += (16.0 - i) / (3 * i);
+    Console.WriteLine($"resultado {i} : {suma}");
+}
+Console.WriteLine("==================================");
+Console.WriteLine($"Resultado final: {resultadoF}");
